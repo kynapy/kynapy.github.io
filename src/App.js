76c2c192517a51b26584ecafd75fa1784fb9b303
Kiyan Ang Ping Young <apyoung11@hotmail.com>
2023-03-07 02:59:26 +0800

    Add Chakra UI

--- src/App.js
+++ src/App.js
@@ -1,28 +1,31 @@
+import { ChakraProvider } from '@chakra-ui/react';
+
 import AboutMe from './AboutMe.js';
 import FAQ from './FAQ.js';
 import Projects from './Projects';
 //import './App.css';
 
-
 function App() {
   return (
-    <div className="App">
-      <nav></nav>
-      <header>
-        <div>
-          <AboutMe /><br></br>
-        </div>
-      </header>
-      <main>
-        <hr></hr>
-        <Projects />
-        <hr></hr>
-        <FAQ />
-      </main>
-      <footer>
+    <ChakraProvider>
+      <div className="App">
+        <nav></nav>
+        <header>
+          <div>
+            <AboutMe /><br></br>
+          </div>
+        </header>
+        <main>
+          <hr></hr>
+          <Projects />
+          <hr></hr>
+          <FAQ />
+        </main>
+        <footer>
 
-      </footer>
-    </div>
+        </footer>
+      </div>
+    </ChakraProvider>
   );
 }
 

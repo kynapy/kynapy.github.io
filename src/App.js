e6160aefb5a0cc0d1270fe7447279a3a2c0182e9
Kiyan Ang Ping Young <apyoung11@hotmail.com>
2024-03-30 01:07:32 +0800

    Fix more formatting

--- src/App.js
+++ src/App.js
@@ -1,8 +1,8 @@
 import { ChakraProvider, useMediaQuery } from '@chakra-ui/react';
 import './App.css';
-import "@fontsource/league-spartan";
-import Aos from "aos";
-import "aos/dist/aos.css";
+import '@fontsource/league-spartan';
+import Aos from 'aos';
+import 'aos/dist/aos.css';
 
 import Navbar from './components/Navbar';
 import AboutSection from './components/AboutSection.js';
@@ -12,34 +12,37 @@ import { useEffect, useRef } from 'react';
 import ContactMeSection from './components/ContactMeSection';
 
 function App() {
-  useEffect(() => {
-    Aos.init({ duration: 1000 });
+    useEffect(() => {
+        Aos.init({ duration: 1000 });
     }, []);
 
-  const projectSection = useRef(null);
-  const experienceSection = useRef(null);
-  const contactSection = useRef(null);
-  const [isMobile] = useMediaQuery("(max-width: 768px)");
+    const projectSection = useRef(null);
+    const experienceSection = useRef(null);
+    const contactSection = useRef(null);
+    const [isMobile] = useMediaQuery('(max-width: 768px)');
 
-  return (
-    <ChakraProvider resetCSS={true}>
-      <div className="App" >
-        <Navbar
-          projectRef={ projectSection }
-          contactRef={ contactSection }
-          experienceRef={ experienceSection }
-        />
-        <AboutSection className="about" isMobile={ isMobile } />
-        <ProjectSection
-          ref={ projectSection }
-          className="projectSection"
-          isMobile={ isMobile }
-        />
-        <ContactMeSection ref={ contactSection } />
-        <Footer />
-      </div>
-    </ChakraProvider>
-  );
+    return (
+        <ChakraProvider resetCSS={true}>
+            <div className='App'>
+                <Navbar
+                    projectRef={projectSection}
+                    contactRef={contactSection}
+                    experienceRef={experienceSection}
+                />
+                <AboutSection
+                    className='about'
+                    isMobile={isMobile}
+                />
+                <ProjectSection
+                    ref={projectSection}
+                    className='projectSection'
+                    isMobile={isMobile}
+                />
+                <ContactMeSection ref={contactSection} />
+                <Footer />
+            </div>
+        </ChakraProvider>
+    );
 }
 
 export default App;
